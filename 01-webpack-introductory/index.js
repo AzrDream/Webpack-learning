0c7f54d74fded2888e11d7f6684fb5cd0f4e9854
AzrDream <1499325862@qq.com>
2022-07-15 11:31:40 +0800

    webpack-打包JS文件

---
+++ 01-webpack-introductory/index.js
@@ -0,0 +1,7 @@
+const hModule = require("./Header");
+const cModule = require("./Content");
+const fModule = require("./Footer");
+
+hModule.addHeader();
+cModule.addContent();
+fModule.addFooter();
