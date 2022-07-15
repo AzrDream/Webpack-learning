c9849515f6aeadcaa8c283a9e3cfccb1e3298a2f
AzrDream <1499325862@qq.com>
2022-07-15 15:18:53 +0800

    webpack-postcss-将px转换为rem

---
+++ 14-webpack-postcss-loader/index.js
@@ -0,0 +1,7 @@
+import icon from "./lnj.jpg";
+import "./index.css";
+
+let oImg = document.createElement("img");
+oImg.src = icon;
+oImg.setAttribute("class", "size");
+document.body.appendChild(oImg);
